ba1717eb8e8eb69513eff4d57bacb49ca64cc410
Shubham Jain <geekshubham@gmail.com>
2021-04-02 11:32:53 -0400

    Added new library, created a request and response method

--- src/main/java/com/shubhamj/nordvpn/api/Client.java
+++ src/main/java/com/shubhamj/nordvpn/api/Client.java
@@ -2,12 +2,13 @@ package com.shubhamj.nordvpn.api;
 
 import com.shubhamj.nordvpn.pojo.HostInfo;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
 public interface Client {
 
-    public void close();
+    public void close() throws IOException;
     public String currentIp();
     public List<String> dnsServers();
     public Map<String, HostInfo> getHostInfo();
--- src/main/java/com/shubhamj/nordvpn/api/NordClient.java
+++ src/main/java/com/shubhamj/nordvpn/api/NordClient.java
@@ -1,21 +1,33 @@
 package com.shubhamj.nordvpn.api;
 
 import com.shubhamj.nordvpn.pojo.HostInfo;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
 import java.util.Map;
 import java.util.logging.Logger;
 
 public class NordClient implements Client {
 
-    private final String API_URL= "https://api.nordvpn.com";
+    private final String API_URL = "https://api.nordvpn.com/";
     private final Logger logger = Logger.getLogger(this.getClass().getName());
+    private final CloseableHttpClient client = HttpClients.createDefault();
 
-    public NordClient() {
+
+    public NordClient() throws IOException {
     }
 
     @Override
-    public void close() {
-
+    public void close() throws IOException {
+        this.client.close();
     }
 
     @Override
@@ -46,5 +58,34 @@ public class NordClient implements Client {
     @Override
     public boolean isCredentialsValid(String username, String password) {
         return false;
+    }
+
+    private String getText(String endPoint) throws IOException {
+        String tR = "";
+
+        endPoint = this.API_URL + endPoint;
+
+        logger.info("Calling " + endPoint);
+
+        CloseableHttpResponse response = getResponse(endPoint);
+        HttpEntity entity = response.getEntity();
+
+        if (entity != null) {
+            tR = EntityUtils.toString(entity);
+        }
+
+        response.close();
+
+        return tR;
+    }
+
+    public CloseableHttpResponse getResponse(String url) throws IOException {
+        HttpGet request = new HttpGet(url);
+        return this.client.execute(request);
+    }
+
+    public static void main(String[] args) throws IOException {
+        NordClient client = new NordClient();
+        client.getText("server/stats");
     }
 }
