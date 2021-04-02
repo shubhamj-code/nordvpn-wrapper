37e063093817ce3996175f762258a126f1d2826c
Shubham Jain <geekshubham@gmail.com>
2021-04-02 11:02:19 -0400

    Added async http request library

--- src/main/java/com/shubhamj/nordvpn/api/NordClient.java
+++ src/main/java/com/shubhamj/nordvpn/api/NordClient.java
@@ -1,11 +1,18 @@
 package com.shubhamj.nordvpn.api;
 
 import com.shubhamj.nordvpn.pojo.HostInfo;
-
 import java.util.List;
 import java.util.Map;
+import java.util.logging.Logger;
 
 public class NordClient implements Client {
+
+    private final String API_URL= "https://api.nordvpn.com";
+    private final Logger logger = Logger.getLogger(this.getClass().getName());
+
+    public NordClient() {
+    }
+
     @Override
     public void close() {
 
