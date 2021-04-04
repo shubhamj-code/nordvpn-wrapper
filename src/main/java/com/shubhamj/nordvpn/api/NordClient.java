4cb68f46c9f3a9a8c942346ef28ff97c8e99494f
Shubham Jain <geekshubham@gmail.com>
2021-04-04 14:52:12 -0400

    Added documentation

--- src/main/java/com/shubhamj/nordvpn/api/NordClient.java
+++ src/main/java/com/shubhamj/nordvpn/api/NordClient.java
@@ -12,33 +12,49 @@ import org.apache.http.util.EntityUtils;
 
 import java.io.IOException;
 import java.util.*;
-import java.util.concurrent.ThreadLocalRandom;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
-public class NordClient implements Client {
+/**
+ * The following class can be used to retrieve public information of NordVPN servers and can be used in your Java
+ * application as a proxy using your own credentials.
+ *
+ * @author Shubham Jain
+ * @version v1.0
+ */
+public class NordClient {
 
     private final String API_URL = "https://api.nordvpn.com/";
     private final Logger logger = Logger.getLogger(this.getClass().getName());
     private final CloseableHttpClient client = HttpClients.createDefault();
     private final ObjectMapper objectMapper = new ObjectMapper();
 
-    @Override
+    /**
+     * Close the existing http connection to the API_URL to release the resource.
+     */
     public void close() throws IOException {
         this.client.close();
     }
 
-    @Override
+    /**
+     * Gets the list of all the available nord vpn servers
+     *
+     * @throws IOException Throws IOException if the retrived json could not be converted to list of nord servers
+     **/
     public List<NordServer> getNordServers() throws IOException {
 
         String nordUrl = API_URL + "server";
         String json = this.getText(nordUrl);
-        return this.objectMapper.readValue(json, new TypeReference<List<NordServer>>() {});
+        return this.objectMapper.readValue(json, new TypeReference<List<NordServer>>() {
+        });
     }
 
-
-    @Override
+    /**
+     * Gets the list of unique countries where nord vpn server exists
+     *
+     * @throws IOException Throws IOException if the nord vpn server list couldn't be retrieved
+     */
     public Set<String> getCountries() throws IOException {
 
         Set<String> countries = new HashSet<>();
@@ -49,7 +65,11 @@ public class NordClient implements Client {
         return countries;
     }
 
-    @Override
+    /**
+     * Fetches all the number of servers available in all the countries
+     *
+     * @throws IOException throws IOException if the nord vpn server list couldn't be retrieved
+     */
     public Map<String, Integer> numberOfServersInCountry() throws IOException {
         Map<String, Integer> serversInCountries = new HashMap<>();
 
@@ -62,7 +82,11 @@ public class NordClient implements Client {
         return serversInCountries;
     }
 
-    @Override
+    /**
+     * Fetches all the available domains of the vpn servers
+     *
+     * @throws IOException throws IOException if the nord vpn server list couldn't be retrieved
+     */
     public List<String> getAvailableVpnServers() throws IOException {
 
         List<String> availableServers = new ArrayList<>();
@@ -71,7 +95,12 @@ public class NordClient implements Client {
 
     }
 
-    @Override
+    /**
+     * Fetches all the available servers of a particular country
+     *
+     * @param flag Requires an ISO code of a particular country
+     * @throws IOException throws IOException if the nord vpn server list couldn't be retrieved
+     */
     public List<String> getVpnServersByCountry(String flag) throws IOException {
 
         return this.getNordServers().stream().filter(n -> n.getFlag().equalsIgnoreCase(flag)).map(NordServer::getDomain)
@@ -79,7 +108,12 @@ public class NordClient implements Client {
 
     }
 
-    @Override
+    /**
+     * Returns a random server from a list of nord vpn servers if a particular server could be a proxy otherwise
+     * returns null
+     *
+     * @throws IOException throws IOException if the nord vpn server list couldn't be retrieved
+     */
     public NordServer getRandomProxy() throws IOException {
         List<NordServer> servers = this.getNordServers();
         Collections.shuffle(servers);
@@ -92,11 +126,21 @@ public class NordClient implements Client {
         return null;
     }
 
-    @Override
+    /**
+     * Returns a list of all the available servers which can be proxy
+     *
+     * @throws IOException throws IOException if the nord vpn server list couldn't be retrieved
+     */
     public List<NordServer> getProxies() throws IOException {
         return this.getNordServers().stream().filter(e -> e.getFeatures().canBeProxy()).collect(Collectors.toList());
     }
 
+    /**
+     * Makes an API call and retrieves the json document from the URL
+     *
+     * @return Returns a json text from the webpage
+     * @throws IOException throws IOException if there was a problem while parsing the JSON from the webpage
+     */
     private String getText(String url) throws IOException {
 
         String tR = "";
